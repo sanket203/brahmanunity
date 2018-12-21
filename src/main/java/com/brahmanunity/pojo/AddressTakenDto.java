5a0875e83d46866b01348b4e99a25494613b7234
sumantd <sumantdharkar@yahoo.com>
2018-12-21 11:40:22 +0530

    Pojo classes are created

---
+++ src/main/java/com/brahmanunity/pojo/AddressTakenDto.java
@@ -0,0 +1,36 @@
+package com.brahmanunity.pojo;
+
+public class AddressTakenDto {
+
+	private int candidateId;
+	private String address;
+	private long contact;
+	private long alternateNumber;
+	
+	public int getCandidateId() {
+		return candidateId;
+	}
+	public void setCandidateId(int candidateId) {
+		this.candidateId = candidateId;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public long getContact() {
+		return contact;
+	}
+	public void setContact(long contact) {
+		this.contact = contact;
+	}
+	public long getAlternateNumber() {
+		return alternateNumber;
+	}
+	public void setAlternateNumber(long alternateNumber) {
+		this.alternateNumber = alternateNumber;
+	}
+	
+	
+}
---
+++ src/main/java/com/brahmanunity/pojo/ProfileAddressDto.java
@@ -0,0 +1,22 @@
+package com.brahmanunity.pojo;
+
+import java.util.List;
+
+public class ProfileAddressDto {
+	
+	List<AddressTakenDto> allAddress;
+	private boolean isAddressExist;
+	
+	public List<AddressTakenDto> getAllAddress() {
+		return allAddress;
+	}
+	public void setAllAddress(List<AddressTakenDto> allAddress) {
+		this.allAddress = allAddress;
+	}
+	public boolean isAddressExist() {
+		return isAddressExist;
+	}
+	public void setAddressExist(boolean isAddressExist) {
+		this.isAddressExist = isAddressExist;
+	}
+}
